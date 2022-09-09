1886c2f8adb4cd5f2c39e26d22eaef8b6dcb9b75
pancetta <sig.pancetta+github@email.com>
2022-09-09 20:02:31 +0000

    Update from https://github.com/Parallel-in-Time/pySDC/commit/f7c134da8379c014daecc16ba2ada6aa4ddda3f8

--- v5/data.js
+++ v5/data.js
@@ -1,43 +1,43 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662751607307,
+  "lastUpdate": 1662753751038,
   "repoUrl": "https://github.com/Parallel-in-Time/pySDC",
   "entries": {
     "pySDC Benchmark with pytest-benchmark": [
       {
         "commit": {
           "author": {
-            "email": "r.speck@fz-juelich.de",
+            "email": "pancetta@users.noreply.github.com",
             "name": "Robert Speck",
             "username": "pancetta"
           },
           "committer": {
-            "email": "r.speck@fz-juelich.de",
-            "name": "Robert Speck",
-            "username": "pancetta"
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
           },
           "distinct": true,
-          "id": "ca20d05139eb9db82db6956b093c2614549f38de",
+          "id": "f7c134da8379c014daecc16ba2ada6aa4ddda3f8",
           "message": "Update ci_pipeline.yml",
-          "timestamp": "2022-09-09T21:03:33+02:00",
-          "tree_id": "69465c42e4894b9afd77e89f4be4d49105c5dc36",
-          "url": "https://github.com/Parallel-in-Time/pySDC/commit/ca20d05139eb9db82db6956b093c2614549f38de"
+          "timestamp": "2022-09-09T21:29:51+02:00",
+          "tree_id": "b6887952453481295543749ed86a84813a42d114",
+          "url": "https://github.com/Parallel-in-Time/pySDC/commit/f7c134da8379c014daecc16ba2ada6aa4ddda3f8"
         },
-        "date": 1662751607289,
+        "date": 1662753751021,
         "tool": "pytest",
         "benches": [
           {
             "name": "pySDC/tests/test_benchmarks/test_PFASST_NumPy.py::test_B",
-            "value": 0.15977622330215133,
+            "value": 0.1540942930053555,
             "unit": "iter/sec",
-            "range": "stddev: 0.018889163295347133",
-            "extra": "mean: 6.258753519970924 sec\nrounds: 5"
+            "range": "stddev: 0.07454235396104912",
+            "extra": "mean: 6.489533002790995 sec\nrounds: 5"
           },
           {
             "name": "pySDC/tests/test_benchmarks/test_collocation.py::test_benchmark_collocation",
-            "value": 3.248400381839205,
+            "value": 3.174907417590241,
             "unit": "iter/sec",
-            "range": "stddev: 0.0012856557891605773",
-            "extra": "mean: 307.8438254073262 msec\nrounds: 5"
+            "range": "stddev: 0.0018325952429769584",
+            "extra": "mean: 314.9698143824935 msec\nrounds: 5"
           }
         ]
       }

af0438d33c557bef6e56a0fc2d14f1fc81becc8e
pancetta <sig.pancetta+github@email.com>
2022-09-10 09:13:43 +0000

    Update from https://github.com/Parallel-in-Time/pySDC/commit/ed0ca7defc86a4fecdae5a6dfe306ba2fc98591c

--- v5/data.js
+++ v5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662753751038,
+  "lastUpdate": 1662801223113,
   "repoUrl": "https://github.com/Parallel-in-Time/pySDC",
   "entries": {
     "pySDC Benchmark with pytest-benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0018325952429769584",
             "extra": "mean: 314.9698143824935 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "r.speck@fz-juelich.de",
+            "name": "Robert Speck",
+            "username": "pancetta"
+          },
+          "committer": {
+            "email": "r.speck@fz-juelich.de",
+            "name": "Robert Speck",
+            "username": "pancetta"
+          },
+          "distinct": true,
+          "id": "ed0ca7defc86a4fecdae5a6dfe306ba2fc98591c",
+          "message": "Update ci_pipeline.yml",
+          "timestamp": "2022-09-10T11:07:43+02:00",
+          "tree_id": "c3c19772d743504f3d63e638836ba01dafb4edbb",
+          "url": "https://github.com/Parallel-in-Time/pySDC/commit/ed0ca7defc86a4fecdae5a6dfe306ba2fc98591c"
+        },
+        "date": 1662801222963,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "pySDC/tests/test_benchmarks/test_PFASST_NumPy.py::test_B",
+            "value": 0.20877328962008315,
+            "unit": "iter/sec",
+            "range": "stddev: 0.018748141857514732",
+            "extra": "mean: 4.789884768400009 sec\nrounds: 5"
+          },
+          {
+            "name": "pySDC/tests/test_benchmarks/test_collocation.py::test_benchmark_collocation",
+            "value": 4.228204283010983,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009316639458479025",
+            "extra": "mean: 236.50702119999778 msec\nrounds: 5"
           }
         ]
       }
